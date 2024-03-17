1f54985b08124e3d6da6dc4888e7b848d638e730
Blank_空白 <312781556@qq.com>
2024-03-18 01:36:28 +0800

    初步框架完成

--- src/components/narbar.vue
+++ src/components/narbar.vue
@@ -1,9 +1,9 @@
 <template>
     <van-tabbar v-model="activeNarbar">
-        <van-tabbar-item name="home" icon="wap-home-o">主页</van-tabbar-item>
-        <van-tabbar-item name="search" icon="search">搜索</van-tabbar-item>
-        <van-tabbar-item name="tool" icon="friends-o">工具</van-tabbar-item>
-        <van-tabbar-item name="setting" icon="setting-o">设置</van-tabbar-item>
+        <van-tabbar-item  to="/home" name="home" icon="wap-home-o">主页</van-tabbar-item>
+        <van-tabbar-item  to="/search" name="search" icon="search">搜索</van-tabbar-item>
+        <van-tabbar-item  to="/toolpage" name="toolpage" icon="friends-o">工具</van-tabbar-item>
+        <van-tabbar-item  to="/settingpage" name="settingpage" icon="setting-o">设置</van-tabbar-item>
     </van-tabbar>
 </template>
 
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -8,10 +8,38 @@ export default [
         path:'/home',
         component:() => import('@/views/layout.vue'),
         children:[
+            // 首页
             {
                 path:'/home',
-                component: () => import('@/views/Home/index.vue')
-            }
+                component: () => import('@/views/Home/index.vue'),
+                meta:{
+                    title:'首页'
+                }
+            },
+            //搜索
+            {
+                path:'/search',
+                component: () => import('@/views/Search/index.vue'),
+                meta:{
+                    title:'搜索'
+                }
+            },
+            //工具页
+            {
+                path:'/toolpage',
+                component: () => import('@/views/ToolPage/index.vue'),
+                meta:{
+                    title:'工具'
+                }
+            },
+            //设置页
+            {
+                path:'/settingpage',
+                component: () => import('@/views/SettingPage/index.vue'),
+                meta:{
+                    title:'设置'
+                }
+            },
         ]
     }
 ]
