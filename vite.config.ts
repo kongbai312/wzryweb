e98b4f76a3302e343f039d0531dcaeae54b1ea8d
Blank_空白 <312781556@qq.com>
2024-03-19 20:34:49 +0800

    配置打包参数

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,59 @@
-import { fileURLToPath, URL } from 'node:url';
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig , loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 //自动引入
-import AutoImport from 'unplugin-auto-import/vite';
+import AutoImport from 'unplugin-auto-import/vite'
 //允许对父传子 props 进行解构
-import ReactivityTransform from '@vue-macros/reactivity-transform/vite';
+import ReactivityTransform from '@vue-macros/reactivity-transform/vite'
 
 //vant自动引入
-import Components from 'unplugin-vue-components/vite';
-import { VantResolver } from 'unplugin-vue-components/resolvers';
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    //允许解构
-    ReactivityTransform(),
-    //自动引入
-    AutoImport({
-      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
-      imports: ['vue'],
-      dts:"src/auto-import.d.ts"
-    }),
-    //项目下src/components下的组件也会被自动注册
-    Components({
-      dts: false, // 关闭自动生成类型声明文件  官网没写这个
-      resolvers: [VantResolver()],
-    })
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ mode } : { mode: string }) => {
+  const env = loadEnv(mode, process.cwd())
+  return defineConfig({
+    plugins: [
+      vue(),
+      //允许解构
+      ReactivityTransform(),
+      //自动引入
+      AutoImport({
+        // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+        imports: ['vue'],
+        dts: 'src/auto-import.d.ts'
+      }),
+      //项目下src/components下的组件也会被自动注册
+      Components({
+        dts: false, // 关闭自动生成类型声明文件  官网没写这个
+        resolvers: [VantResolver()]
+      })
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      open: true,
+      port: 8080
+    },
+    //github部署
+    base: env.MODE === 'production' ? '/wzryweb/' : './',
+    // 打包配置
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      sourcemap: false,
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true
+        }
+      }
     }
-  },
-  server:{
-    open:true,
-    port:8080,
-  }
-})
+  })
+}
