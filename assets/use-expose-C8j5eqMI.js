b92d8bcbdbce943743c8c853d7c69472c689673a
kongbai312 <86761381+kongbai312@users.noreply.github.com>
2024-03-25 14:09:55 +0000

    Deploying to release from @ kongbai312/wzryweb@04a4d8298aef4d8f9fe8453cb02507834e0dceed 🚀

---
+++ assets/use-expose-C8j5eqMI.js
@@ -0,0 +1,1 @@
+import{e as t,b as s}from"./index-CBCQ6Rvf.js";function r(n){const e=s();e&&t(e.proxy,n)}export{r as u};
---
+++ assets/use-id-gROM3XTf.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index-CBCQ6Rvf.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return`${t}-${++r}`}export{u};
