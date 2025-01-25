35b99446d0fcab27516c03fac052bcbd86d478c0
kongbai312 <86761381+kongbai312@users.noreply.github.com>
2025-01-25 09:46:07 +0000

    Deploying to release from @ kongbai312/wzryweb@5a72dfd1a63a86a19d7cc0675ec25fec0c180ae2 🚀

--- assets/use-expose-B7WTVG09.js
+++
@@ -1,1 +0,0 @@
-import{x as t,ai as s}from"./index-Con2zwDB.js";function a(n){const e=s();e&&t(e.proxy,n)}export{a as u};
---
+++ assets/use-id-Bia0DeqN.js
@@ -0,0 +1,1 @@
+import{ai as t}from"./index-M4brf8cj.js";let r=0;function o(){const n=t(),{name:e="unknown"}=(n==null?void 0:n.type)||{};return`${e}-${++r}`}export{o as u};
